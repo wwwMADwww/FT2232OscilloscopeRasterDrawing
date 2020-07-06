4304d1a84912237fe673bd73abdabff6eabac38a
MAD <4d4144h@gmail.com>
2020-07-06 23:23:52 +0300

    Add project files.

---
+++ ft2232raster/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ft2232raster
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
